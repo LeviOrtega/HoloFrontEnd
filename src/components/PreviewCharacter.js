b2ddcacd38325b888d3f58d223d023152ed9cceb
LeviOrtega <leviortega@mymail.mines.edu>
2021-11-20 20:12:44 -0700

    Added resume info to preview

--- src/components/PreviewCharacter.js
+++ src/components/PreviewCharacter.js
@@ -9,8 +9,18 @@ class PreviewCharacter extends React.Component {
     this.state = {
       previewID: this.props.previewID,
       character: null,
+      resumeField: null,
     };
   }
+
+  // getResumeFields(rFields){
+  //   const fields = []
+
+  //   for(let i = 0; i < rFields.length; i++){
+  //     fields.push(
+  //       <
+  //     )
+  //   }
 
   componentDidMount() {
     getDoc(doc(firestore, "creations", this.state.previewID)).then(
@@ -24,24 +34,6 @@ class PreviewCharacter extends React.Component {
                 className="preview-wrapper"
                 style={{ display: "flex", flexDirection: "column" }}
               >
-                <div
-                  className="preview-character-container"
-                  style={{
-                    backgroundColor: characterSnap.data().backgroundColor,
-                    border: "white",
-                    borderStyle: "solid",
-                    borderRadius: "10%",
-                    marginTop: "1%",
-                  }}
-                >
-                  <Character
-                    bladeColor={characterSnap.data().bladeColor}
-                    hiltColor={characterSnap.data().hiltColor}
-                    eyeColor={characterSnap.data().eyeColor}
-                    robeColor={characterSnap.data().robeColor}
-                    skinColor={characterSnap.data().skinColor}
-                  />
-                </div>
                 <div
                   className="title-wrapper"
                   style={{
@@ -64,6 +56,93 @@ class PreviewCharacter extends React.Component {
                     {characterSnap.data().charTitle}
                   </label>
                 </div>
+
+                <div
+                  className="preview-character-container"
+                  style={{
+                    backgroundColor: characterSnap.data().backgroundColor,
+                    border: "white",
+                    borderStyle: "solid",
+                    borderRadius: "10%",
+                    marginTop: "1%",
+                  }}
+                >
+                  <div className ="character-hover-wrapper ">
+                  <Character
+                    bladeColor={characterSnap.data().bladeColor}
+                    hiltColor={characterSnap.data().hiltColor}
+                    eyeColor={characterSnap.data().eyeColor}
+                    robeColor={characterSnap.data().robeColor}
+                    skinColor={characterSnap.data().skinColor}
+                  />
+                  </div>
+                </div>
+              </div>
+            ),
+
+            resumeField: (
+              <div>
+                <h2 style={{textAlign:"center", color:"white"}}>Resume Info</h2>
+                <form>
+                  {characterSnap.data().formValues.map((element, index) => (
+                    <div
+                      className="form-inline"
+                      key={index}
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "flex-start",
+                      }}
+                    >
+                      <input
+                        className="char-title"
+                        placeholder={"Title"}
+                        type="text"
+                        name="desc"
+                        value={element.desc || ""}
+                        style={{
+                          textAlign: "center",
+                          width: "50%",
+                          alignSelf: "center",
+                        }}
+                      />
+
+                      <textarea
+                        className="char-title"
+                        type="text"
+                        name="detail"
+                        value={element.detail || ""}
+                        style={{ height: "200px", resize: "none" }}
+                      />
+                    </div>
+                  ))}
+                </form>
+                <h3 style={{textAlign:"center", color:"white"}}>Contact Info</h3>
+                <form style={{ textAlign: "center" }}>
+                  <input
+                    className="char-title"
+                    type="text"
+                    name="real-name"
+                    value={characterSnap.data().realName}
+                    style={{ textAlign: "center", width: "50%" }}
+                  />
+                  <input
+                    className="char-title"
+                    type="text"
+                    name="contact-info"
+                    value={characterSnap.data().contactInfo}
+                    style={{ textAlign: "center", width: "80%" }}
+                  />
+                  <input
+                    className="char-title"
+                    type="text"
+                    name="website"
+                    value={characterSnap.data().website}
+                    style={{ textAlign: "center", width: "100%" }}
+                  />
+                </form>
+
+                
               </div>
             ),
           });
@@ -73,7 +152,19 @@ class PreviewCharacter extends React.Component {
   }
 
   render() {
-    return <div style={{ width: "20%" }}>{this.state.character}</div>;
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ width: "50%" }}>{this.state.character}</div>
+        <div style={{}}>{this.state.resumeField}</div>
+      </div>
+    );
   }
 }
 
