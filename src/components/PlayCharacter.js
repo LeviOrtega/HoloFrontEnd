b6d0ddeb3ab68447bc2f499eb6f05855c56925e2
LeviOrtega <leviortega@mymail.mines.edu>
2021-11-26 14:34:09 -0800

    Finished up play resume, removed the contact info from the play so it looks more sleek, added holocrons

--- src/components/PlayCharacter.js
+++ src/components/PlayCharacter.js
@@ -3,21 +3,21 @@ import { doc, getDoc } from "firebase/firestore";
 import { firestore } from "../Firebase";
 import Character from "../resources/Character";
 import Typewriter from "typewriter-effect";
+import Sith, { Jedi } from "../resources/Holocron";
 
 class PlayCharacter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      backgroundColor: "#111111",
       previewID: this.props.previewID,
       character: null,
-      contactInfo: null,
       resumeField: [],
       resumeTitle: [],
       resumeTitleText: "",
-      resumeFieldText: "",
-      resumeIndex: 0,
       loading: true,
-      onClick: this.props.onClick
+      onClick: this.props.onClick,
+      isSith: false,
     };
   }
 
@@ -31,18 +31,9 @@ class PlayCharacter extends React.Component {
             character: (
               <div
                 className="preview-wrapper"
-                style={{ display: "flex", flexDirection: "column" }}
+                style={{ display: "flex", flexDirection: "column", rowGap: 0 }}
               >
-                <div
-                  className="preview-character-container"
-                  style={{
-                    backgroundColor: characterSnap.data().backgroundColor,
-                    border: "white",
-                    borderStyle: "solid",
-                    borderRadius: "10%",
-                    marginTop: "1%",
-                  }}
-                >
+                <div className="preview-character-container">
                   <div className="character-hover-wrapper ">
                     <Character
                       bladeColor={characterSnap.data().bladeColor}
@@ -56,36 +47,11 @@ class PlayCharacter extends React.Component {
               </div>
             ),
 
-            contactInfo: (
-              <form style={{ textAlign: "center" }}>
-                <div style={{ display: "flex", flexDirection: "row" }}>
-                  <input
-                    readOnly
-                    className="preview-text-field"
-                    type="text"
-                    name="real-name"
-                    value={characterSnap.data().realName}
-                    style={{ textAlign: "center", width: "auto" }}
-                  />
-                  <input
-                    readOnly
-                    className="preview-text-field"
-                    type="text"
-                    name="contact-info"
-                    value={characterSnap.data().contactInfo}
-                    style={{ textAlign: "center", width: "auto" }}
-                  />
-                  <input
-                    readOnly
-                    className="preview-text-field"
-                    type="text"
-                    name="website"
-                    value={characterSnap.data().website}
-                    style={{ textAlign: "center", width: "auto" }}
-                  />
-                </div>
-              </form>
-            ),
+           
+          });
+          this.setState({
+            backgroundColor: characterSnap.data().backgroundColor,
+            isSith: characterSnap.data().isSith,
           });
         }
 
@@ -111,64 +77,85 @@ class PlayCharacter extends React.Component {
 
   render() {
     return (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: "1%",
-          color: "white",
-          width: "100%",
-        }}
-      >
-        <div style={{ width: "20%" }}>{this.state.character}</div>
+      <>
+        <div
+          style={{
+            backgroundColor: this.state.backgroundColor,
+            border: "white",
+            borderStyle: "solid",
+            borderRadius: "10%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "1%",
+            color: "white",
+            width: "75%",
+          }}
+        >
+          <div
+            style={{ display: "flex", flexDirection: "column", width: "30%", marginRight: "2%"}}
+          >
+            <div style={{}}>{this.state.character}</div>
+
+            {!this.state.loading && (
+              <div>{this.state.isSith ? <Sith /> : <Jedi />}</div>
+            )}
+          </div>
+
+          {!this.state.loading && (
+            <div style={{ textAlign: "left", width: "40%", margin: "10px" }}>
+              <div style={{ fontSize: "40px", marginBottom: "10px" }}>
+                {this.state.resumeTitleText}
+              </div>
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .start()
+                    .callFunction(() => {
+                      this.updateResumeInfo(0);
+                    })
+                    .typeString(this.state.resumeField[0])
+
+                    .pauseFor(5000)
+                    .deleteAll(1)
+                    .callFunction(() => {
+                      this.updateResumeInfo(1);
+                    })
+                    .typeString(this.state.resumeField[1])
+
+                    .pauseFor(this.state.resumeField[1] ? 5000 : 0)
+                    .deleteAll(1)
+                    .callFunction(() => {
+                      this.updateResumeInfo(2);
+                    })
+                    .typeString(this.state.resumeField[2])
+                    .pauseFor(this.state.resumeField[2] ? 5000 : 0)
+                    .deleteAll(1)
+                    .callFunction(() => {
+                      this.setState({
+                        resumeTitleText: "",
+
+                      });
+
+                      this.state.onClick()
+                    });
+                }}
+              />
+            </div>
+          )}
+        </div>
 
         {!this.state.loading && (
-          <div style={{ textAlign: "left", width: "40%", margin: "10px" }}>
-            <div style={{fontSize:"40px", marginBottom: "10px"}}>{this.state.resumeTitleText}</div>
-            <Typewriter
-
-              onInit={(typewriter) => {
-                typewriter
-                  .start()
-                  .callFunction(() => {
-                    this.updateResumeInfo(0);
-                  })
-                  .typeString(this.state.resumeField[0])
-
-                  .pauseFor(5000)
-                  .deleteAll(1)
-                  .callFunction(() => {
-                    this.updateResumeInfo(1);
-                  })
-                  .typeString(this.state.resumeField[1])
-
-                  .pauseFor(this.state.resumeField[1] ? 5000 : 0)
-                  .deleteAll(1)
-                  .callFunction(() => {
-                    this.updateResumeInfo(2);
-                  })
-                  .typeString(this.state.resumeField[2])
-                  .pauseFor(this.state.resumeField[2] ? 5000 : 0)
-                  .deleteAll(1)
-                  .callFunction(() => {
-                    this.setState({
-                        resumeTitleText:""
-                      });
-                  })
-              }}
-            />
-          </div>
+          <button
+            className="publish"
+            style={{ marginBottom: "%" }}
+            onClick={() => this.state.onClick()}
+          >
+            Preview Full Resume
+          </button>
         )}
-
-        <div style={{}}>{this.state.contactInfo}</div>
-
-        {!this.state.loading &&
-        <button className="publish" style={{marginBottom:"2%"}} onClick={() => this.state.onClick() }>
-        Preview Full Resume
-      </button>}
-      </div>
+      </>
     );
   }
 }
